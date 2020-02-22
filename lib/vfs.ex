d419eaaafc654630c7d1be5bee4cc333927430bf
Bolek Kurowski <bolek.kurowski@gmail.com>
2020-02-22 18:20:44 -0500

    mix new vfs

---
+++ lib/vfs.ex
@@ -0,0 +1,18 @@
+defmodule Vfs do
+  @moduledoc """
+  Documentation for `Vfs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vfs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vfs_test.exs
@@ -0,0 +1,8 @@
+defmodule VfsTest do
+  use ExUnit.Case
+  doctest Vfs
+
+  test "greets the world" do
+    assert Vfs.hello() == :world
+  end
+end
